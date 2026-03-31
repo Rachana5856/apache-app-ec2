96f4bf8a5938c7f44a1f47636b77b085aef6dbba
root <root@ip-10-0-2-42.ap-south-1.compute.internal>
2026-03-31 14:10:19 +0000

    third commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "exam" {
+resource "aws_instance" "exam1" {
   ami           = "ami-0931307dcdc2a28c9"   # Amazon Linux 2 (ap-south-1)
   instance_type = "t2.micro"
 
