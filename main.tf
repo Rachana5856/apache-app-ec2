29ffc378a26d3c38e2225c05c97164d4c646001e
root <root@ip-10-0-2-42.ap-south-1.compute.internal>
2026-03-31 14:16:22 +0000

    fourth commit

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "exam1" {
   ami           = "ami-0931307dcdc2a28c9"   # Amazon Linux 2 (ap-south-1)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name        = "rk-ec2"
